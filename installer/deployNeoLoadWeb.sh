8be6ba4d1b6f4fbbf17a817768d261f9662cce9b
hrexed <henrik.rexed@neotys.com>
2019-06-21 11:04:32 +0200

    Version 1 of the Keptn NeoLoad Service

--- installer/deployNeoLoadWeb.sh
+++ installer/deployNeoLoadWeb.sh
@@ -14,5 +14,5 @@ NL_SERVICE_RELEASE="0.0.1"
 
 print_info "Deploying neoload-service $NL_SERVICE_RELEASE"
 # to update the link
-kubectl apply -f https://raw.githubusercontent.com/keptn/dynatrace-service/$DT_SERVICE_RELEASE/dynatrace-service.yaml
+kubectl apply -f https://raw.githubusercontent.com/keptn-contrib/neoload-service/$NL_SERVICE_RELEASE/config/neoloadexecutor/k8s-neoload-service-manifes.yml
 verify_kubectl $? "Deploying neoload-service failed."
