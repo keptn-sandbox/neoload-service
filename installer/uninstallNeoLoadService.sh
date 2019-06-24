68cc919def61baf42cd06638bb55bc18eaffdbdd
hrexed <henrik.rexed@neotys.com>
2019-06-24 18:37:38 +0200

    Version 1 of the Keptn NeoLoad Service

---
+++ installer/uninstallNeoLoadService.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+kubectl delete secret neoload -n keptn --ignore-not-found
+
+# Create dynatrace-service
+NL_SERVICE_RELEASE="0.0.1"
+
+print_info "Delete neoload-service $NL_SERVICE_RELEASE"
+# to update the link
+kubectl delete -f https://raw.githubusercontent.com/keptn-contrib/neoload-service/$NL_SERVICE_RELEASE/config/neoloadexecutor/k8s-neoload-service-manifes.yml --ignore-not-found
