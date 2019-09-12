9b2fd2355dacec5c3782377baee288864c49f160
hrexed <henrik.rexed@neotys.com>
2019-09-12 18:30:13 +0200

    change the deployment script of keptn 0.4.0

--- installer/deployNeoLoadWeb.sh
+++ installer/deployNeoLoadWeb.sh
@@ -14,5 +14,5 @@ NL_SERVICE_RELEASE="0.0.5"
 
 print_info "Deploying neoload-service $NL_SERVICE_RELEASE"
 # to update the link
-kubectl apply -f https://raw.githubusercontent.com/keptn-contrib/neoload-service/$NL_SERVICE_RELEASE/config/neoloadexecutor/k8s-neoload-service-manifes.yml
+kubectl apply -f https://raw.githubusercontent.com/keptn-contrib/neoload-service/$NL_SERVICE_RELEASE/config/neoloadexecutor/neoload-service-manifest.yml
 verify_kubectl $? "Deploying neoload-service failed."
