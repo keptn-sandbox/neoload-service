9473c01d1d74ad0a5a4b587675adb88cb3e258f0
hrexed <henrik.rexed@neotys.com>
2019-12-06 10:45:36 +0100

    Change the service to be aligned with keptdn 0.6.0

--- installer/uninstallNeoLoadService.sh
+++ installer/uninstallNeoLoadService.sh
@@ -6,5 +6,5 @@ NL_SERVICE_RELEASE="0.6.0"
 
 print_info "Delete neoload-service $NL_SERVICE_RELEASE"
 # to update the link
-kubectl delete -f https://raw.githubusercontent.com/keptn-contrib/neoload-service/$NL_SERVICE_RELEASE/config/neoloadexecutor/servcice.yaml --ignore-not-found
+kubectl delete -f https://raw.githubusercontent.com/keptn-contrib/neoload-service/$NL_SERVICE_RELEASE/config/neoloadexecutor/service.yaml --ignore-not-found
 kubectl delete -f https://raw.githubusercontent.com/keptn-contrib/neoload-service/$NL_SERVICE_RELEASE/config/neoloadexecutor/distributor.yaml --ignore-not-found
