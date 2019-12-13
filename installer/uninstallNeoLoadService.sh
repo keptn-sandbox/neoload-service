45e65739980439662760de20ba4598153e5181e2
hrexed <henrik.rexed@neotys.com>
2019-12-13 09:34:43 +0100

    change deployment file

--- installer/uninstallNeoLoadService.sh
+++ installer/uninstallNeoLoadService.sh
@@ -4,7 +4,7 @@ kubectl delete secret neoload -n keptn --ignore-not-found
 # Create dynatrace-service
 NL_SERVICE_RELEASE="0.6.0"
 
-print_info "Delete neoload-service $NL_SERVICE_RELEASE"
+echo "Delete neoload-service $NL_SERVICE_RELEASE"
 # to update the link
 kubectl delete -f https://raw.githubusercontent.com/keptn-contrib/neoload-service/$NL_SERVICE_RELEASE/config/neoloadexecutor/service.yaml --ignore-not-found
 kubectl delete -f https://raw.githubusercontent.com/keptn-contrib/neoload-service/$NL_SERVICE_RELEASE/config/neoloadexecutor/distributor.yaml --ignore-not-found
